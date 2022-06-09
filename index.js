c5f2c38baefa16297efc32ea1b51c80d6650765c
Bonface Masira <bonfacemasira@gmail.com>
2022-06-09 15:45:12 +0300

    commit tree 62bcb749bbfde6bf39c737f0d20b38c29183dc6e

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift();
+}
+
+function appendCat(name){
+    const catsX = [...cats, 'Broom'];
+    return catsX;
+}
+
+function prependCat(name) {
+    const catsY = ['Arnold', ...cats];
+    return catsY;
+}
+
+function removeLastCat() {
+    const removeLastCatHere = cats.slice(0, cats.length -1 )
+    return removeLastCatHere;
+}
+
+function removeFirstCat(){
+    const removeFirstCatHere = cats.slice(1);
+    return removeFirstCatHere;
+}
